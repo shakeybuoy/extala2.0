d16a944dc2b9be9ff912d94efbbc0d8f2a2fe416
shakeybuoy <abhi.sharma.0873@gmail.com>
2022-06-12 23:53:28 +0530

    link fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ function Navbar() {
     return (
         <nav className="navbar z-20 bg-one-primary fixed top-0 left-0 lg:px-20 drop-shadow-lg">
             <div className="navbar-start ">
-                <img className="w-32 rounded-md cursor-pointer" src="./Extala_Logo_B.JPG" />
+                <img className="w-32 rounded-md cursor-pointer" src="../Extala_Logo_B.JPG" />
             </div>
         </nav>
     )
